72dcb5b32c66d44a6f42498ecfbb161d15bd2c32
Sarah Porzig <sarahporzig@icloud.com>
2016-08-16 11:39:04 +1000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  var chevronDown = function() {
+    $(".chevron-down").css("top", $("#navImage").height());
+  };
+  setInterval(chevronDown, 1);
+
+  var block1 = function() {
+    $("#block1").css("top", $("#navImage").height());
+  };
+  setInterval(block1, 1);
+
+  var block2 = function() {
+    $("#block2").css("top", $("#navImage").height());
+  };
+  setInterval(block2, 1);
+
+  $(".chevron-down").click(function() {
+$('html, body').animate({
+    scrollTop: $("#block1").offset().top
+  }, 800);
+});
+
+$(".myProjectsButton").click(function() {
+$('html, body').animate({
+  scrollTop: $("#block1").offset().top
+}, 800);
+});
+});
+
+$(function() {
+  $('[data-toggle="popover"]').popover();
+});
+
+$('.nav a').on('click', function(){
+    $('.navbar-toggle').click()
+});
