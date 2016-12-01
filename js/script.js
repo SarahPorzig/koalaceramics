7ef5f418233b8450b976efc0fff36055dc77f405
Sarah Porzig <sarahporzig@icloud.com>
2016-12-01 18:40:49 +1100

    overhaul of site

--- js/script.js
+++ js/script.js
@@ -1,30 +1,14 @@
 $(document).ready(function() {
 
-  var chevronDown = function() {
-    $(".chevron-down").css("top", $("#navImage").height());
-  };
-  setInterval(chevronDown, 1);
-
-  var block1 = function() {
-    $("#block1").css("top", $("#navImage").height());
-  };
-  setInterval(block1, 1);
-
-  var block2 = function() {
-    $("#block2").css("top", $("#navImage").height());
-  };
-  setInterval(block2, 1);
-
-  $(".chevron-down").click(function() {
-    $('html, body').animate({
-    scrollTop: $("#block1").offset().top
-    }, 800);
-  });
-
   $(".myProjectsButton").click(function() {
     $('html, body').animate({
-      scrollTop: $("#block1").offset().top
-    }, 800);
+      scrollTop: $("#myProjects").offset().top - 100
+    }, 400);
+  });
+  $(".aboutMeButton").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#aboutMe").offset().top - 100
+    }, 400);
   });
   $('.nav a').on('click', function(){
       $('.navbar-toggle').click()
